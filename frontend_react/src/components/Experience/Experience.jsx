64d18ba21fc40de2dd929a3c9a08a7382dadeab1
Jeremy Martinez <39386502+Jaydotcom27@users.noreply.github.com>
2023-02-19 20:50:15 -0500

    Created base timeline and experience component

--- frontend_react/src/components/Experience/Experience.jsx
+++ frontend_react/src/components/Experience/Experience.jsx
@@ -4,8 +4,18 @@ import './Experience.scss'
 const Experience = ({ title, subtitle, content, images}) => {
   return (
     <div id='Experience'>
-        <div className='test'>a</div>
-        <span>{title}</span>
+      <span className='timeline-dot'></span>
+      <div className='d-flex inner-container'>
+        <div className='header'>
+          <span className='title'>{title} | </span><span className='subtitle'>{subtitle}</span>
+        </div>
+        <span className='content'>{content}</span>
+        <div className='d-flex image-container'>
+          {images.map((image) => (
+            <div className='image'>{image}</div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
--- frontend_react/src/container/Timeline/Timeline.jsx
+++ frontend_react/src/container/Timeline/Timeline.jsx
@@ -6,22 +6,49 @@ const Timeline = () => {
     const [ experiences ] = useState([{
         title: 'Title 1',
         subtitle: 'Subtitle 1',
-        content: 'Content 1',
-        images: ['url1','url2']
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        images: []
 
     },
     {
         title: 'Title 2',
         subtitle: 'Subtitle 2',
-        content: 'Content 2',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
         images: ['url1','url2']
 
     },
     {
         title: 'Title 3',
         subtitle: 'Subtitle 3',
-        content: 'Content 3',
-        images: ['url1','url2']
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        images: []
+    },
+    {
+        title: 'Title 4',
+        subtitle: 'Subtitle 4',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam.',
+        images: []
+
+    },
+    {
+        title: 'Title 5',
+        subtitle: 'Subtitle 5',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        images: []
+
+    },
+    {
+        title: 'Title 6',
+        subtitle: 'Subtitle 6',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        images: ['url1']
+
+    },
+    {
+        title: 'Title 7',
+        subtitle: 'Subtitle 7',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        images: ['url1', 'url2']
 
     }]);
 
@@ -29,9 +56,10 @@ const Timeline = () => {
         <div id='Timeline'>
             {experiences.map((experience) => (
                 <Experience 
+                    key = { experience.title }
                     title = { experience.title }
                     subtitle = { experience.subtitle }
-                    conent = { experience.content }
+                    content = { experience.content }
                     images = { experience.images }
                 />
             ))}
