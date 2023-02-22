91bc508be65be4ce7f223681dda148232250ae89
Jeremy Martinez <jaymartinezsw@gmail.com>
2023-02-22 09:30:12 -0500

    Made progress with the dynamic content in the Items component

--- frontend_react/src/components/Item/Item.jsx
+++ frontend_react/src/components/Item/Item.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import { React } from 'react'
 import { FaPaintBrush } from 'react-icons/fa'
 import './Item.scss'
 
-const Item = () => {
+const Item = ({title, subtitle, type, content, skills}) => {
   return (
     <div id='Item'>
       <div className='item__inner'>
         <div className='item__header'>
           <div className='item__header-pair'>
-            <span className='item__title'>Portfolio Item Title</span>
+            <span className='item__title'>{title}</span>
             <span className='item__pill'>
-              <span className='pill__title'>Creative</span>
+              <span className='pill__title'>{type}</span>
               <FaPaintBrush />
             </span>
           </div>
-          <span className='item__subtitle'>Portfolio Item SubTitle</span>
+          <span className='item__subtitle'>{subtitle}</span>
         </div>
         <div className='item__body'>
-          <span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, blanditiis sapiente similique officia nam nemo sit facere voluptatum aliquam repellendus iste nostrum consequuntur explicabo, officiis assumenda placeat, culpa esse fugit! </span>
+          <span>{content}</span>
         </div>
         <div className='item__skills'>
-          
+          {skills.map((skill) => (
+            <div>
+              <div>{skill}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- frontend_react/src/pages/Portfolio/Portfolio.jsx
+++ frontend_react/src/pages/Portfolio/Portfolio.jsx
@@ -1,15 +1,58 @@
-import React from 'react'
-import './Portfolio.scss'
+import React, { useState } from 'react'
 import { Item } from '../../components'
+import './Portfolio.scss'
 
 const Portfolio = () => {
+  const [ items ] = useState([{
+    title: 'Title',
+    subtitle: 'Subtitle',
+    type: 'Creative',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    skills: ['Adobe XD', 'Blender', 'Research', 'Javascript']
+  },
+  {
+    title: 'Title',
+    subtitle: 'Subtitle',
+    type: 'Creative',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    skills: ['Adobe XD', 'Blender', 'Research', 'Javascript']
+  },
+  {
+    title: 'Title',
+    subtitle: 'Subtitle',
+    type: 'Creative',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    skills: ['Adobe XD', 'Blender', 'Research', 'Javascript']
+  },
+  {
+    title: 'Title',
+    subtitle: 'Subtitle',
+    type: 'Creative',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    skills: ['Adobe XD', 'Blender', 'Research', 'Javascript']
+  },
+  {
+    title: 'Title',
+    subtitle: 'Subtitle',
+    type: 'Creative',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    skills: ['Adobe XD', 'Blender', 'Research', 'Javascript']
+  },
+])
+
   return (
     <div id='Portfolio'>
         <div className='items-container'>
-            <Item></Item>
-            <Item></Item>
-            <Item></Item>
-            <Item></Item>
+          { items.map((item) => (
+            <Item
+              key = {item.title}
+              title = {item.title}
+              subtitle = {item.subtitle}
+              type = {item.type}
+              content = {item.content}
+              skills = {item.skills}
+            ></Item>
+          ))}
         </div>
     </div>
   )
