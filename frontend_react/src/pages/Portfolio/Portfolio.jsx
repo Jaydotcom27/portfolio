eda69f21a92664395df5045d3b5624ba62bc1d8a
Jeremy Martinez <jaymartinezsw@gmail.com>
2023-03-02 17:40:43 -0500

    plane progress

--- frontend_react/src/pages/Portfolio/Portfolio.jsx
+++ frontend_react/src/pages/Portfolio/Portfolio.jsx
@@ -1,8 +1,23 @@
 import React, { useState } from 'react'
 import { Item } from '../../components'
+import { BsFilterRight } from 'react-icons/bs'
 import './Portfolio.scss'
 
 const Portfolio = () => {
+  const [ activeFilter, setActiveFilter ] = useState('');
+
+  const filterOptions = [
+    { filter: 'creative' },
+    { filter: 'engineering' },
+    { filter: 'data' },
+    { filter: 'research' },
+  ];
+
+const handleFilterClick = (filter) => {
+  setActiveFilter(filter.filter);
+  console.log(activeFilter)
+};
+
   const [ items ] = useState([{
     title: 'Title',
     subtitle: 'Subtitle',
@@ -42,6 +57,16 @@ const Portfolio = () => {
 
   return (
     <div id='Portfolio'>
+        <span className='filters-container'>
+          <BsFilterRight className='filter-active'/>
+          <span className='filter-active'>Show me</span>
+          { filterOptions.map((option) => (
+            <span 
+              className={activeFilter === option.filter ? 'filter-active' : 'filter'} 
+              onClick={() => handleFilterClick(option)}>{option.filter}</span>
+          ))}
+          <span className='filter-active'>things</span>
+        </span>
         <div className='items-container'>
           { items.map((item) => (
             <Item
